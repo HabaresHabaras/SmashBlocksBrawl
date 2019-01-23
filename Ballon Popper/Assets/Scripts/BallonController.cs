4a94525e5027e547a2c2bf17bafd2445d85b954d
Dali Gala <habaresbit@gmail.com>
2019-01-23 01:17:40 -0500

    A bunch of stuff

---
+++ Ballon Popper/Assets/Scripts/BallonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallonController : MonoBehaviour {
+    //how much it grows each time
+    public float scaleFactor = 1.2f;
+
+    //maximum scale
+    public float maxScale = 3;
+
+    void Start(){
+        if(scaleFactor <= 1){
+            print("It's too small");
+        }
+    }
+    void OnMouseDown(){
+        //access the scale   &&      //increase this value
+        //class transform from the sphere in Unity
+        transform.localScale *= scaleFactor;
+        //check if we've passed the maxScale
+
+        if(transform.localScale.x >= maxScale){
+            //if so destroy the ballon
+            Destroy(gameObject);
+
+        }
+
+
+        //else not do aything
+
+    }
+
+
+}
