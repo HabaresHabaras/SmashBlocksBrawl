673c1de7bbb69290e919b232d12702ad87e5295e
Dali Gala <habaresbit@gmail.com>
2019-01-23 01:19:30 -0500

    New challenges I made before the prototype game

---
+++ Vector Movement/Assets/Scripts/VectorMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * Vector3.up;
+        //We want our cube to move up and down between y = -10 and y =10
+        //if we reached y = 10 and we were moving up, then we now move down
+        if(transform.position.y >= 10 && speed > 0)
+        {
+            speed *= -1;
+        }    
+        else if(transform.position.y <= -10 && speed < 0){
+            speed *= -1;
+        }
+        // also, if we reached y = -10 and were moving down, then 
+    }
+}
