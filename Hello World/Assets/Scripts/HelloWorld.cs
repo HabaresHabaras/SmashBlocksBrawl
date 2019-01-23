15bdf4d18f684b0bfd7734c51b14f42f02029fd0
Dali Gala <habaresbit@gmail.com>
2019-01-23 01:20:57 -0500

    Hello world file with my first unity project

---
+++ Hello World/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //DATATYPES
+    // int / float / string / bool
+    void Start()
+    {
+        //variable declaration
+        string playerName;
+
+        //variable assignment
+        playerName = "Mr Pink";
+
+        //show that to user
+        print(playerName);
+
+        // variable declaration + assignment
+        // string playerName = "Mr Pink";
+        string enemyName = "Miss Orange";
+
+        //show that 
+        print(enemyName);
+
+        //energy
+        float energy = 1.5f;  //f is needed when declaring floats, else the number will be considered a double and be given a huge ammount of decimal zeroes
+
+        //num of lives
+        int n = 10;
+
+        //if player is alive
+        bool isAlive = true;
+
+        //Arithmetic operations 
+        //Total score of a user
+        int levelsCompleted = 10;
+        int totalScore = levelsCompleted * 100;
+
+        float total = 100 + 1.5f;
+
+        int lives = 5 - 1;
+
+        float parts = 100 / 50;
+
+        float combination = (100 + 1.5f) * 10 / 100;
+
+        print("The result for "+ playerName + " is " + combination);
+
+        float celsius = 5;
+
+        float fahreheit = celsius * 9/5 + 32;
+        print("The result is " + fahreheit);
+
+
+
+
+
+    }
+}
---
+++ Hello World/Assets/Scripts/classes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintGame();
+
+    }
+
+        void PrintGame(){
+            print("Vr RPG");
+        }
+        public string myGameIdea = "Awesome bro";
+    
+}
